04250af0407d35b80f7c7daf410e4699440af69a
Matt Parmett <1214074+mattparmett@users.noreply.github.com>
2024-03-23 12:40:30 -0400

    Add support for storing constants in chunks.

--- src/chunk.h
+++ src/chunk.h
@@ -2,6 +2,7 @@
 #define clox_chunk_h
 
 #include "common.h"
+#include "value.h"
 
 // One-byte operation code (opcode)
 // Controls the type of instruction (add, subtract, lookup, etc.)
@@ -14,10 +15,12 @@ typedef struct {
   int count;        // number of allocated entries in use
   int capacity;     // number of allocated entries
   uint8_t* code;
+  ValueArray constants; // literals stored as array
 } Chunk;
 
 void initChunk(Chunk* chunk);
 void freeChunk(Chunk* chunk);
 void writeChunk(Chunk* chunk, uint8_t byte);
+int addConstant(Chunk* chunk, Value value);
 
 #endif
